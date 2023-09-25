1c817533c761a33bab152a4bab7abede6c091d20
fachi aditia <fachiaditia1809@gmail.com>
2023-09-25 11:45:03 +0300

    Tugas Daspro 1

---
+++ pertemuan 1.py
@@ -0,0 +1,61 @@
+# a=10 
+# print(a)
+
+# b="saya suka makan"
+# print(b)
+# print(type(a),type(b))
+
+# c=str(a)
+# print(c,type(c))
+
+# d=int(a)
+# print(d,type(d))
+
+nama=input("masukkan nama:")
+nim=int(input("masukkan nim:"))
+print(nama,nim)
+print(nama,",",nim)
+print(type(nim))
+print("nama:",nama)
+print("nim:",nim)
+
+print(f" hai nama aku {nama}. aku kuliah di samarinda. nim saya {nim}")
+
+# a=int(input ("masukkan angka"))
+# b=5
+# kali=a*b
+# tambah=a+b
+# bagi=a/b
+# kurang=a-b
+# modulus=a%b
+
+# print(kali,tambah,bagi,kurang,modulus)
+
+# a=int("masukkan angka:")
+# if (a==100):
+#    print("variabel a adalah 100")
+# else:
+#    print("variabel a lain 100")
+
+# a=input("masukkan kata-kata:")
+# if (a=="hujan"):
+#    print("basah")
+# else:
+#    print("kering")
+
+# print("PROGRAM MENGHITUNG VOLUME KUBUS\n")
+# print("fiki")
+
+# sisi=int(input("masukkan sisi kubus:"))
+# volumekubus= sisi*sisi*sisi
+
+# print(f"volume kubus adalah: {volumekubus}")
+
+# print("PROGRAM MENGHITUNG KELILING LINGKARAN\n")
+
+# diameter=int(input("masukkan diameter lingkaran:"))
+# phi=3.14
+# kelilinglingkaran=2*phi*diameter
+# print(int(kelilinglingkaran))
+
+# print(f"keliling lingkaran adalah:{kelilinglingkaran}")
